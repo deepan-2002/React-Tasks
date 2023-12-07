7ea8f01bb10800ef741dc8775330a627a63ee83d
Deeban Yathiraja R <deebanyathiraja2002@gmail.com>
2023-12-07 12:31:29 +0530

    navbar modified

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -6,24 +6,27 @@ import './logo512.png'
 function Menu() {
     return (
         <>
-            <Navbar bg="dark" data-bs-theme="dark">
+            <Navbar collapseOnSelect expand="lg" bg="dark" data-bs-theme="dark">
                 <Container fluid>
                     <Navbar.Brand href="/"><img src='./logo512.png' alt='Logo' width={'30px'}></img></Navbar.Brand>
-                    <Nav className="m-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/congrats">Congrats Card</Nav.Link>
-                        <Nav.Link href="/superover">Super Over</Nav.Link>
-                        <Nav.Link href="/socialbuttons">Social Buttons</Nav.Link>
-                        <Nav.Link href="/notification">Notifications</Nav.Link>
-                        <Nav.Link href="/logindesign">Login Design</Nav.Link>
-                        <Nav.Link href="/tech">Technology Cards</Nav.Link>
-                        <Nav.Link href="/hook">Hook</Nav.Link>
-                        <Nav.Link href="/fruit">Fruits</Nav.Link>
-                        <Nav.Link href="/feedback">Feedback</Nav.Link>
-                        <Nav.Link href="/datefunction">Date function</Nav.Link>
-                        <Nav.Link href="/fetch">Fetch</Nav.Link>
-                        <Nav.Link href="/productlist">Products</Nav.Link>
-                    </Nav>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="m-auto">
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link href="/congrats">Congrats Card</Nav.Link>
+                            <Nav.Link href="/superover">Super Over</Nav.Link>
+                            <Nav.Link href="/socialbuttons">Social Buttons</Nav.Link>
+                            <Nav.Link href="/notification">Notifications</Nav.Link>
+                            <Nav.Link href="/logindesign">Login Design</Nav.Link>
+                            <Nav.Link href="/tech">Technology Cards</Nav.Link>
+                            <Nav.Link href="/hook">Hook</Nav.Link>
+                            <Nav.Link href="/fruit">Fruits</Nav.Link>
+                            <Nav.Link href="/feedback">Feedback</Nav.Link>
+                            <Nav.Link href="/datefunction">Date function</Nav.Link>
+                            <Nav.Link href="/fetch">Fetch</Nav.Link>
+                            <Nav.Link href="/productlist">Products</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </>
